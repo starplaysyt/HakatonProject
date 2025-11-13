0f3cb40ae39c59c578cacdffdd38a65f40bbc2ab
RealMuka <andreyfedosuk66@gmail.com>
2025-11-13 15:00:47 +0300

    API: ADD: UserController + GetUserByUserName
    add user controller + add method to get user  by username

--- HakatonProject/Models/Entities/Event.cs
+++ HakatonProject/Models/Entities/Event.cs
@@ -1,6 +1,7 @@
 public class Event : IEntity
 {
     public string Name { get; set; }
+    public string Type { get; set;}
     public DateTime TimeStart { get; set; }
     public DateTime TimeEnd { get; set; }
     public List<Place> Places { get; set; }
