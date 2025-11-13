3d2cc3481799aae197e616cf45464e2705b61c8b
starplaysyt <horaudmitry@gmail.com>
2025-11-13 22:26:03 +0300

    UserEventRepository changed

--- HakatonProject/Models/Repositories/UserEventRepository.cs
+++ HakatonProject/Models/Repositories/UserEventRepository.cs
@@ -3,28 +3,16 @@ using Microsoft.EntityFrameworkCore;
 
 namespace HakatonProject.Models.Repositories;
 
-public enum UserEventRepositoryErrors
-{
-    None = 0,
-    UserNotFound = 1,
-    EventNotFound = 2
-}
-
 public class UserEventRepository(ApplicationDataDbContext _dbContext)
 {
     private readonly ApplicationDataDbContext dbContext = _dbContext;
     
-    public async Task<Event[]> TryGetUserEvents(User user)
-    {
-        var events = await dbContext.UserEvents.Where(ue => ue.User.Id == user.Id).Select(ue => ue.Event).ToArrayAsync();
-        return events;
-    }
+    public async Task<Event[]> TryGetUserEvents(User user) =>
+        await dbContext.UserEvents.Where(ue => ue.User.Id == user.Id).Select(ue => ue.Event).ToArrayAsync();
+
     
-    private async Task<User[]> TryGetEventUsers(Event e)
-    {
-        var users = await dbContext.UserEvents.Where(ue => ue.Event.Id == e.Id).Select(ue => ue.User).ToArrayAsync();
-        return users;
-    }
+    private async Task<User[]> TryGetEventUsers(Event e) =>
+        await dbContext.UserEvents.Where(ue => ue.Event.Id == e.Id).Select(ue => ue.User).ToArrayAsync();
 
     private async Task TryAddEvent(Event e, User user) => await dbContext.UserEvents.AddAsync(
         new UserEvent
