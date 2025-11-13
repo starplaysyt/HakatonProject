6169bf41445e03a77f6190dcd64e827c274247eb
starplaysyt <horaudmitry@gmail.com>
2025-11-13 22:24:29 +0300

    PlaceRepository added

---
+++ HakatonProject/Models/Repositories/PlaceRepository.cs
@@ -0,0 +1,19 @@
+﻿using HakatonProject.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace HakatonProject.Models.Repositories;
+
+public class PlaceRepository(ApplicationDataDbContext _dbContext)
+{
+    private readonly ApplicationDataDbContext dbContext = _dbContext;
+
+    public async Task<Place[]> GetAllPlaces() => await dbContext.Places.ToArrayAsync();
+    
+    public async Task<Place?> GetPlace(long id) => await dbContext.Places.FirstOrDefaultAsync(p => p.Id == id);
+    
+    public async Task AddPlace(Place place)
+    {
+        await dbContext.Places.AddAsync(place);
+        await dbContext.SaveChangesAsync();
+    } 
+}
