a996161ade0507d0441ebb5643b304d165a4b2ee
RealMuka <andreyfedosuk66@gmail.com>
2025-11-13 22:44:28 +0300

    API:add dto folder and CreateEventDTO

---
+++ HakatonProject/Controllers/DTOs/CreateEventDTO.cs
@@ -0,0 +1,7 @@
+public class CreateEventDTO
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public DateTime TimeStart { get; set; }
+    public DateTime TimeEnd { get; set; }
+}
--- HakatonProject/Controllers/EventController.cs
+++ HakatonProject/Controllers/EventController.cs
@@ -40,11 +40,3 @@ public class EventController : ControllerBase
         }
     }
 }
-
-public class CreateEventDTO
-{
-    public string Name { get; set; }
-    public string Description { get; set; }
-    public DateTime TimeStart { get; set; }
-    public DateTime TimeEnd { get; set; }
-}
